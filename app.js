a3e0579314f8e7061abbd04c33a650324110f5ef
Denis Sokolinskiy <coolstrong-mc@yandex.ru>
2017-10-19 19:54:47 +0300

    First

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ *@callback TimeFunction
+ *@param {number} time
+ *@returns {number} value 
+*/
+
+/**
+ * @function
+ * Draws graph from two coordinate functions
+ * @param {TimeFunction} funcX
+ * @param {TimeFunction} funcY
+ */
+function draw(funcX, funcY) {
+    //TODO
+}
+
