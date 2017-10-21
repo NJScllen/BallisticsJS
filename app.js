908b4ab88448f6038bc5410e39a8fdc477e67e18
Denis Sokolinskiy <coolstrong-mc@yandex.ru>
2017-10-21 16:10:50 +0300

    Migrated to snapsvg

--- app.js
+++ app.js
@@ -1,32 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var svg = require("svg.js");
-var graphUtils = require("./graphUtils");
 /**
  * Draws graph from two coordinate functions.
  * @function draw
  * @param {TimeFunction} funcX function, providing x coordinate
  * @param {TimeFunction} funcY function, providing y coordinate
- * @param {svgjs.Doc} canvas canvas to draw on
+ * @param {Snap.Paper} canvas canvas to draw on
+ * @param {graphUtils.Size} size size of the canvas
  * @param {number} fields fields around the canvas
+ * @returns {Snap.Element} drawn path
  */
-function draw(funcX, funcY, canvas, fields) {
+function draw(funcX, funcY, canvas, size, fields) {
     if (fields === void 0) { fields = 10; }
-    var w = canvas.width() - (fields * 2);
-    var h = canvas.height();
-    var pathArr = [];
-    graphUtils.drawAxis(canvas, fields);
-    pathArr.push(['M', fields, fields]);
-    for (var i = 0; i < w; i++) {
-        var x = funcX(i);
-        var y = h - funcY(i);
-        var cpx = pathArr[pathArr.length - 1][pathArr.length - 2];
-        pathArr.push(['Q', cpx, y - fields, x + fields, y - fields]);
-        if (x > 0 && y === 0) {
-            break;
-        }
-    }
-    var path = new svg.PathArray(pathArr);
-    canvas.path(path).attr({ fill: "#4286f4" });
-    return canvas;
+    throw new Error("Not implemented");
 }
--- graphUtils.js
+++ graphUtils.js
@@ -3,11 +3,37 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * Draws two axis on provided canvas.
  * @function drawAxis
- * @param {svgjs.Doc} canvas canvas to draw on
+ * @param {Snap.Paper} canvas canvas to draw on
+ * @param {Size} size size of the canvas
  * @param {number} fields fields around the canvas
+ * @returns {Axis} axis object, containing both axis
  */
-function drawAxis(canvas, fields) {
-    //TODO
-    return canvas;
+function drawAxis(canvas, size, fields) {
+    if (fields === void 0) { fields = 10; }
+    var axisparams = {
+        "stroke-width": "2",
+        "stroke": "#000"
+    };
+    var x = canvas.line(fields - 2, size.height - fields, size.width - fields, size.height - fields).attr(axisparams);
+    var y = canvas.line(fields, fields - 2, fields, fields).attr(axisparams);
+    var xa = canvas.polyline([size.width - fields - 3, size.height - fields - 2,
+        size.width - fields, size.height - fields,
+        size.width - fields - 3, size.height - fields + 2]).attr(axisparams);
+    var ya = canvas.polyline([fields - 2, fields + 3, fields, fields, fields + 2, fields + 3]).attr(axisparams);
+    return { axisX: x, axisY: y, arrowX: xa, arrowY: ya };
 }
 exports.drawAxis = drawAxis;
+/**
+ * Draws markers showing coordinates of the specific point
+ * @function drawScale
+ * @param {Snap.Paper} canvas canvas to draw on
+ * @param {Point} point point which needs to be marked
+ * @param {number} fields fields around the canvas
+ * @returns {Axis} axis object, containing lines to both axis
+ */
+function drawScale(canvas, point, fields) {
+    //TODO
+    if (fields === void 0) { fields = 10; }
+    throw new Error("Not implemented");
+}
+exports.drawScale = drawScale;
