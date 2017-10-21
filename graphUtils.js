0eed69850bc744e27dde57478162e8b3884e582a
Denis Sokolinskiy <coolstrong-mc@yandex.ru>
2017-10-21 20:34:09 +0300

    drawing algorithm (alpha)

--- graphUtils.js
+++ graphUtils.js
@@ -8,18 +8,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
  * @param {number} fields fields around the canvas
  * @returns {Axis} axis object, containing both axis
  */
-function drawAxis(canvas, size, fields) {
-    if (fields === void 0) { fields = 10; }
-    var axisparams = {
+function drawAxis(canvas, size, fields = 10) {
+    let axisparams = {
         "stroke-width": "2",
-        "stroke": "#000"
+        "stroke": "#000",
+        "stroke-opacity": "0.7"
     };
-    var x = canvas.line(fields - 2, size.height - fields, size.width - fields, size.height - fields).attr(axisparams);
-    var y = canvas.line(fields, fields - 2, fields, fields).attr(axisparams);
-    var xa = canvas.polyline([size.width - fields - 3, size.height - fields - 2,
+    let x = canvas.line(fields - 2, size.height - fields, size.width - fields, size.height - fields).attr(axisparams);
+    let y = canvas.line(fields, fields - 2, fields, fields).attr(axisparams);
+    let xa = canvas.polyline([size.width - fields - 3, size.height - fields - 2,
         size.width - fields, size.height - fields,
         size.width - fields - 3, size.height - fields + 2]).attr(axisparams);
-    var ya = canvas.polyline([fields - 2, fields + 3, fields, fields, fields + 2, fields + 3]).attr(axisparams);
+    let ya = canvas.polyline([fields - 2, fields + 3, fields, fields, fields + 2, fields + 3]).attr(axisparams);
     return { axisX: x, axisY: y, arrowX: xa, arrowY: ya };
 }
 exports.drawAxis = drawAxis;
@@ -31,9 +31,8 @@ exports.drawAxis = drawAxis;
  * @param {number} fields fields around the canvas
  * @returns {Axis} axis object, containing lines to both axis
  */
-function drawScale(canvas, point, fields) {
+function drawScale(canvas, point, fields = 10) {
     //TODO
-    if (fields === void 0) { fields = 10; }
     throw new Error("Not implemented");
 }
 exports.drawScale = drawScale;
